b6d7fd26e460ef360c2c3c0b7e45514874bde1f5
Lewis Renfrew <LewisRe@linn.co.uk>
2021-11-17 11:52:50 +0000

    add material ui

--- Content/components/Sample.jsx
+++ Content/components/Sample.jsx
@@ -1,8 +1,9 @@
-﻿var React = require('react');
+﻿import React from 'react';
+import Typography from '@mui/material/Typography';
 
 
 export default function Sample({name}) {
-        return <div>
-                <h1>Hello {name} I am a react component that was rendered on the server :)</h1>
-            </div>;
+        return <Typography variant="h1">
+                Hello {name} I am a react component that was rendered on the server :)
+            </Typography>;
 }
